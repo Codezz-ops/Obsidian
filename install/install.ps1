608e42835d9237507f8afeec44ddde8cef181ca5
Codezz-ops <codezz-ops@protonmail.com>
2024-01-16 19:25:36 -0500

    Fixed install.ps1

--- install/install.ps1
+++ install/install.ps1
@@ -1,3 +1,3 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; `iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco install cmake make mingw
